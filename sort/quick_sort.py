e2daddb6757c8b3f9de3b66ab85f729b6d59cc31
Lewis Kabui <lewis.maina@andela.com>
2018-01-05 16:03:06 +0300

    [Feature] Update quick sort algorithm

--- sort/quick_sort.py
+++ sort/quick_sort.py
@@ -1,29 +1,24 @@
-sorted_array = []
-def quick_sort(array):
-    largers, smallers, pivot = [], [], array[0]
-    if len(array) > 1:
-        array.pop(0)
-        for no in array:
-            if no > pivot:
-                largers.append(no)
-            elif no <= pivot:
-                smallers.append(no)
-        if len(smallers) > 0:
-            quick_sort(smallers)
-        sorted_array.append(pivot)
-        if len(largers) > 0:
-            quick_sort(largers)
-    elif len(array) == 1:
-        sorted_array.append(pivot)
-        return pivot
+def partition(array, first, last):
 
-array = [-182, 139, 163, -183, 4, 166, -89, -158, 14, 19, 39, -194, 197, -103, 
-76, 165, -147, 74, -76, 70, -13, -81, 9, -14, -140, -189, -13, -128, 190, 152, 
--23, 171, -34, 87, -119, 196, -15, -176, 44, 155]
+    pivot = array[last]
+
+    wall = first
+
+    for index in range(first, last):
+        if array[index] < pivot:
+            array[index], array[wall] = array[wall], array[index]
+            wall += 1
+
+    array.insert(wall, array.pop(last))
+
+    return wall
 
 
-print("-" * 80)
-print("Before: {0}".format(array))
-quick_sort(array)
-print("After: {0}".format(sorted_array))
-print("-" * 80)
+def quick_sort(array, first, last):
+
+    if first < last:
+        pivot = partition(array, first, last)
+        quick_sort(array, first, pivot - 1)
+        quick_sort(array, pivot + 1, last)
+
+    return array
---
+++ tests/test_quick_sort.py
@@ -0,0 +1,48 @@
+import unittest, random
+
+from sort import quick_sort
+
+class TestQuickSort(unittest.TestCase):
+
+    def test_quick_sort_on_array_size_100(self):
+        a100 = range(0,100)
+
+        random.shuffle(a100)
+
+        r_a100 = quick_sort.quick_sort(a100, 0, len(a100) - 1)
+
+        random_index = random.randint(0, 99)
+
+        self.assertEqual(random_index, r_a100[random_index])
+
+    def test_quick_sort_on_reversed_array_of_size_100(self):
+        a100 = range(99, -1, -1)
+        
+        r_a100 = quick_sort.quick_sort(a100, 0, len(a100) - 1)
+
+        random_index = random.randint(0, 99)
+
+        self.assertEqual(random_index, r_a100[random_index])
+
+    def test_quick_sort_on_array_of_size_100_containing_negative_integers(self):
+        a100 = range(-99, 1)
+
+        random.shuffle(a100)
+
+        r_a100 = quick_sort.quick_sort(a100, 0, len(a100) - 1)
+
+        random_index = random.randint(0, 99)
+
+        self.assertEqual(random_index - 99, r_a100[random_index])
+
+    def test_every_element_is_sorted(self):
+        a20 = range(0, 20)
+
+        random.shuffle(a20)
+
+        result = quick_sort.quick_sort(a20, 0, len(a20) - 1)
+
+        ideal = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+
+        self.assertEqual(result, ideal)
+
