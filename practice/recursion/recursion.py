da9e1cd04e8bc3c2eefe5aedd37dfa9de4bc1359
Lewis Kabui <lewisemm@users.noreply.github.com>
2020-08-31 23:29:11 +0300

    Add recursion practice examples

---
+++ practice/recursion/recursion.py
@@ -0,0 +1,36 @@
+def reverse_string(string):
+    """
+    Using recursion to reverse a string.
+    """
+    if (len(string) < 2):
+        return string
+    return reverse_string(string[1::]) + string[0]
+
+# print(reverse_string('recursion'))
+
+def print_array(array, index=0):
+    """
+    Using recursion to print all the elements of an array.
+    """
+    if (index >= len(array)):
+        return
+    print('array[{}] is {}'.format(index, array[index]))
+    print_array(array, index + 1)
+
+# array = [3, 4, 1, 8, 2, 6, 5, 7, 9]
+# print_array(array)
+
+def is_palindrome(string, left_index, right_index):
+    """
+    Using recursion to determine if a string is palindrome.
+    """
+    if len(string) < 1:
+        return True
+    if string[left_index] != string[right_index]:
+        return False
+    if left_index == right_index or left_index > right_index:
+        return True
+    return is_palindrome(string, left_index + 1, right_index - 1)
+
+string = 'racecar'
+print(is_palindrome(string, 0, len(string) - 1))
