da118ae473015c2d49886bfbce25748b7cc6f510
Lewis Kabui <lewisemm@users.noreply.github.com>
2020-09-23 21:59:09 +0300

    Create a binary tree

---
+++ trees/binary_tree.py
@@ -0,0 +1,95 @@
+class Node(object):
+    """
+    This class represents a node in a binary tree.
+    """
+
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+    
+    def is_leaf(self):
+        return self.left == self.right == None
+    
+    def navigate(self, value):
+        if value > self.value:
+            return self.right
+        elif value <= self.value:
+            return self.left
+    
+    def __repr__(self):
+        return '<Node: {}>'.format(self.value)
+
+
+class BinaryTree(object):
+    """
+    This class encapsulates a binary tree and contains methods to perform
+    CRUD operations on it.
+    """
+
+    def __init__(self):
+        self.root = None
+
+
+    def insert(self, value):
+        """
+        Adds a node to the binary tree.
+        """
+        if self.root is None:
+            self.root = Node(value)
+        else:
+            current_node = self.root
+            while current_node:
+                temp = current_node.navigate(value)
+                if  temp is not None:
+                    current_node = temp
+                else:
+                    break
+            if current_node.value >= value:
+                current_node.left = Node(value)
+            else:
+                current_node.right = Node(value)
+    
+    def search(self, value):
+        """
+        Searches the binary tree for a node that contains the value `value`.
+
+        Returns `None` if value isn't found in the binary tree.
+        """
+        current_node = self.root
+        while current_node:
+            if current_node.value == value:
+                return current_node
+            temp = current_node.navigate(value)
+            if temp is not None:
+                current_node = temp
+            else:
+                return None
+            
+        
+    def breadth_first(self):
+        """
+        Display all the nodes in the binary tree using the breadth first
+        traversal.
+        """
+        q = [self.root]
+        while len(q):
+            current = q.pop(0)
+            print(current.value)
+            if current.left:
+                q.append(current.left)
+            if current.right:
+                q.append(current.right)
+            
+
+b = BinaryTree()
+b.insert(37)
+b.insert(61)
+b.insert(62)
+b.insert(32)
+b.insert(31)
+b.insert(33)
+b.insert(32)
+b.breadth_first()
+print('Results for Search 42: {}'.format(b.search(42)))
+print('Results for Search 33: {}'.format(b.search(33)))
