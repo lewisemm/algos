6f5edd51a73948654cc3506b7864e013a0e429f7
Lewis Kabui <lewisemm@users.noreply.github.com>
2022-02-19 23:50:11 +0300

    Create and test delete and shrink functionality

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -46,6 +46,33 @@ class HashTable:
                 return v
         raise KeyError(f"'{key}'")
 
+    def delete(self, key):
+        """
+        Creates a hash using `key` and `self.prime` and uses this hash to locate
+        the (key, value) entry in `self.array` for removal.
+        """
+        used_space = self.capacity - self.free_space
+        if used_space <= self.capacity / 4 and self.capacity > self.DEFAULT_SIZE:
+            self.__shrink_hashtable(used_space)
+        hashed = key % self.prime
+        element = self.array[hashed]
+        if element == None:
+            raise KeyError(f"'{key}'")
+        to_be_popped = None
+        found = False
+
+        for index, pair in enumerate(element):
+            k, v = pair
+            if k == key:
+                to_be_popped = index
+                found = True
+                break
+        if found:
+            self.array[hashed].pop(to_be_popped)
+            self.free_space += 1
+            return
+        raise KeyError(f"'{key}'")
+
     def __grow_hashtable(self):
         """
         Doubles the size of `self.array` when `self.capacity` is exceeded.
@@ -53,6 +80,26 @@ class HashTable:
         self.free_space = self.capacity
         self.capacity *= 2
         self.prime = generate_prime(self.capacity)
+        new_array = [None] * self.capacity
+        for element in self.array:
+            if element:
+                for key, value in element:
+                    hashed = key % self.prime
+                    if new_array[hashed] == None:
+                        new_array[hashed] = [(key, value)]
+                    else:
+                        new_array[hashed].append((key, value))
+        self.array = new_array
+        del new_array
+
+    def __shrink_hashtable(self, used):
+        """
+        Shrinks the size of `self.array` by half when 3/4 of `self.capacity` is
+        unused.
+        """
+        self.capacity //= 2
+        self.prime = generate_prime(self.capacity)
+        self.free_space = self.capacity - used
         new_array = [None] * self.capacity
         for element in self.array:
             if element:
--- tests/hashtable/test_hashtable.py
+++ tests/hashtable/test_hashtable.py
@@ -61,6 +61,25 @@ class HashTableTest(unittest.TestCase):
         for k, v in mapping:
             self.assertEqual(self.hashtable.get(k), v)
 
+    def test_hashtable_delete(self):
+        """
+        Test delete operation on hashtable.
+        """
+        a = [no for no in range(10)]
+        random.shuffle(a)
+        b = [no for no in range(10, 20)]
+        random.shuffle(b)
+        mapping = {}
+        for pair in zip(a, b):
+            key, value = pair
+            self.hashtable.insert(key, value)
+            mapping[pair] = True
+        for key, value in mapping:
+            self.hashtable.delete(key)
+            # key does not exist and any attempt to access it should raise
+            # KeyError
+            with self.assertRaises(KeyError):
+                self.hashtable.delete(key)
 
     def test_hashtable_overwrite_with_insert(self):
         """
@@ -83,3 +102,54 @@ class HashTableTest(unittest.TestCase):
             self.hashtable.insert(key, value)
             self.assertNotEqual(self.hashtable.get(key), mapping[key])
             self.assertEqual(self.hashtable.get(key), value)
+
+    def test_hashtable_shrink_when_three_quarter_space_unused(self):
+        """
+        Test that the hashtable shrinks in size when the total free space is
+        >= 3/4 and capacity > self.hashtable.DEFAULY_SIZE
+        """
+        self.assertEqual(self.hashtable.capacity, 8)
+        self.assertEqual(len(self.hashtable.array), 8)
+        a = [no for no in range(10)]
+        random.shuffle(a)
+        b = [no for no in range(10, 20)]
+        random.shuffle(b)
+        mapping = {}
+        for pair in zip(a, b):
+            key, value = pair
+            self.hashtable.insert(key, value)
+            mapping[key] = value
+        self.assertEqual(self.hashtable.capacity, 16)
+        self.assertEqual(len(self.hashtable.array), 16)
+        # hashtable now has 10 items, capacity is 16. Delete 6 items to get 12
+        # free spaces (12/16 == 3/4 free space).
+        count = 0
+        for key in mapping:
+            if count == 7:
+                break
+            self.hashtable.delete(key)
+            count += 1
+        self.assertEqual(self.hashtable.capacity, 8)
+        self.assertEqual(len(self.hashtable.array), 8)
+
+    def test_hashtable_not_shrink_when_free_space_is_three_quarters(self):
+        """
+        Test that the hashtable does not shrink when total free space is 3/4
+        and capacity == self.hashtable.DEFAULT_SIZE (8).
+        """
+        self.assertEqual(self.hashtable.capacity, 8)
+        self.assertEqual(len(self.hashtable.array), 8)
+        # add 3 items, free space == 5/8
+        to_be_deleted = None
+        for i in range(3):
+            key, value = int(random.random() * 100), int(random.random() * 100)
+            self.hashtable.insert(key, value)
+            to_be_deleted = key
+        # delete 1 item, free space = 6/8 (i.e. 3/4)
+        self.hashtable.delete(to_be_deleted)
+        # assert key has been deleted
+        with self.assertRaises(KeyError):
+            self.hashtable.get(to_be_deleted)
+        # assert hashtable has not shrunk
+        self.assertEqual(self.hashtable.capacity, 8)
+        self.assertEqual(len(self.hashtable.array), 8)
