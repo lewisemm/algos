55500dbb198751c33092daae3a3708780b1b6e22
Lewis Kabui <lewisemm@users.noreply.github.com>
2022-02-28 14:27:52 +0300

    Add AVL rotation mechanisms

--- trees/bst/avl.py
+++ trees/bst/avl.py
@@ -28,3 +28,77 @@ class AVL:
             parent.left = node
         else:
             parent.right = node
+
+    def left_left_rotation(self, node, parent, grand_parent):
+        branch = None
+        if grand_parent:
+            if parent == grand_parent.left:
+                branch = 'L'
+            elif parent == grand_parent.right:
+                branch = 'R'
+        node_right = node.right
+        node.right = parent
+        parent.left = node_right
+        if branch == 'L':
+            grand_parent.left = node
+        elif branch == 'R':
+            grand_parent.right = node
+        else:
+            # branch is None and node is the new root.
+            # insert 50, 60, 30, 40, 20, 10 respectively to test this.
+            self.root = node
+
+    def left_right_rotation(self, node, parent, grand_parent):
+        branch = None
+        if grand_parent:
+            if parent == grand_parent.left:
+                branch = 'L'
+            elif parent == grand_parent.right:
+                branch = 'R'
+        node_left = node.left
+        node.left = parent
+        parent.right = node_left
+        if branch == 'L':
+            grand_parent.left = node
+        elif branch == 'R':
+            grand_parent.right = node
+        else:
+            # branch is None and node is the new root.
+            # insert 40, 20, 5, 30, 25 respectively to test this.
+            self.root = node
+
+    def right_right_rotation(self, node, parent, grand_parent):
+        branch = None
+        if grand_parent:
+            if parent == grand_parent.left:
+                branch = 'L'
+            elif parent == grand_parent.right:
+                branch = 'R'
+        node_left = node.left
+        node.left = parent
+        parent.right = node_left
+        if branch == 'L':
+            grand_parent.left = node
+        elif branch == 'R':
+            grand_parent.right = node
+        else:
+            # branch is None and node is the new root.
+            # insert 50, 60, 70 respectively to test this
+            self.root = node
+
+    def right_left_rotation(self, node, parent, grand_parent):
+        branch = None
+        if grand_parent:
+            if parent == grand_parent.left:
+                branch = 'L'
+            elif parent == grand_parent.right:
+                branch = 'R'
+        node_right = node.right
+        node.right = parent
+        parent.left = node_right
+        if branch == 'L':
+            grand_parent.left = node
+        elif branch == 'R':
+            grand_parent.right = node
+        else:
+            self.root = node
