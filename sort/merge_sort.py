53e8197cb7e750543815fd20039a344db16c25af
Lewis Kabui <lewisemm@users.noreply.github.com>
2022-08-25 20:38:34 +0300

    Track index using int variables
    - Eliminate the use of pop() on the first element because this
    will cause successive elements in the array to shift one position
    to the left for each pop operation
    - Use int variables instead to keep track of sorted and unsorted
    boundary of the arrays

--- sort/merge_sort.py
+++ sort/merge_sort.py
@@ -3,18 +3,24 @@ import random
 def merger(left, right):
 
     sorted_array = []
+    # left index, right index, left_length, right_length
+    li, ri, left_length, right_length = 0, 0, len(left), len(right)
 
-    while right and left and len(left) and len(right):
-        if left[0] <= right[0]:
-            sorted_array.append(left.pop(0))
+    while li < left_length and ri < right_length:
+        if left[li] <= right[ri]:
+            sorted_array.append(left[li])
+            li += 1
         else:
-            sorted_array.append(right.pop(0))
+            sorted_array.append(right[ri])
+            ri += 1
 
-    while left:
-        sorted_array.append(left.pop(0))
+    while li < left_length:
+        sorted_array.append(left[li])
+        li += 1
 
-    while right:
-        sorted_array.append(right.pop(0))
+    while ri < right_length:
+        sorted_array.append(right[ri])
+        ri += 1
 
     return sorted_array
             
