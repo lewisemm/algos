a58e18d074205b1843e399e9df48921c8ff12122
Lewis Kabui <lewisemm@users.noreply.github.com>
2022-08-25 20:37:40 +0300

    Test merge sort with large randomised array

--- tests/sort/test_merge_sort.py
+++ tests/sort/test_merge_sort.py
@@ -1,6 +1,7 @@
 import unittest, random
 
 from sort import merge_sort
+from tests.sort import helper
 
 class TestMergeSort(unittest.TestCase):
 
@@ -46,3 +47,9 @@ class TestMergeSort(unittest.TestCase):
         ideal = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
 
         self.assertEqual(result, ideal)
+
+    def test_mergesort_on_randomised_array_of_million_items(self):
+        width, scale = 10**6, 10**6
+        array = helper.create_randomised_array(width, scale)
+        newly_sorted = merge_sort.merge_sort(array)
+        self.assertEqual(newly_sorted, sorted(array))
