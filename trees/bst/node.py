70823ffe8a934f71b9b43acea256d389f2214a1f
Lewis Kabui <lewisemm@users.noreply.github.com>
2022-09-05 22:21:02 +0300

    Update TreeNode
    - Change class name to TreeNode to clear ambiguity between this class
    and similar classes found in LinkedList etc
    - Add less than, less than or equal to and equal to customizations
    to avoid the use of "node.val > current.val" comparisons inside trees.

--- trees/bst/node.py
+++ trees/bst/node.py
@@ -1,8 +1,20 @@
-class Node:
-    def __init__(self, key):
-        self.key = key
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
         self.left = None
         self.right = None
 
+    def is_leaf(self):
+        return self.left == self.right == None
+
+    def __lt__(self, other):
+        return self.val < other.val
+
+    def __le__(self, other):
+        return self.val <= other.val
+
+    def __eq__(self, other):
+        return id(self) == id(other)
+
     def __repr__(self):
-        return f'<Node: {self.key}>'
+        return f'<TreeNode: {self.val}>'
