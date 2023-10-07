b075479c04b96aff2896743a472ac16680145c58
Lewis M. Kabui <lewisemm@users.noreply.github.com>
2023-10-07 21:37:35 +0300

    Add tests for sorting arrays with -ve integers

--- tests/sort/test_radix_sort.py
+++ tests/sort/test_radix_sort.py
@@ -52,3 +52,23 @@ class TestRadixSort(unittest.TestCase):
         array = helper.create_randomised_array(width, scale)
         radix_sorted = radix_sort.radix_sort(array)
         self.assertEqual(radix_sorted, sorted(array))
+
+    def test_radix_sort_on_array_of_size_100_containing_negative_integers(self):
+        a100 = [no for no in range(-99, 1)]
+
+        random.shuffle(a100)
+
+        radix_sorted = radix_sort.radix_sort(a100)
+
+        random_index = random.randint(0, 99)
+
+        self.assertEqual(random_index - 99, radix_sorted[random_index])
+        self.assertEqual(radix_sorted, [no for no in range(-99, 1)])
+
+    def test_radix_sort_on_positives_and_negatives_in_array_of_million_items(self):
+        # width - array of one million values
+        # scale - integers in the array are of values between 0 - 100
+        width, scale = 10**6, 10**2
+        array = helper.create_randomised_array_with_negatives(width, scale)
+        radix_sorted = radix_sort.radix_sort(array)
+        self.assertEqual(radix_sorted, sorted(array))
