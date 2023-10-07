8f633dbe13c37ae90c9a47e157231c7fc69e596c
Lewis M. Kabui <lewisemm@users.noreply.github.com>
2023-10-07 21:33:53 +0300

    Add function to genenerate +ve and -ve values

--- tests/sort/helper.py
+++ tests/sort/helper.py
@@ -9,3 +9,17 @@ def create_randomised_array(width=10, scale=10):
     for i in range(width):
         array.append(int(random.random() * scale))
     return array
+
+def create_randomised_array_with_negatives(width=10, scale=10):
+    """
+    Creates an array of length `width` that contains a random mix of positive
+    and negative values between 0 and scale.
+    """
+    array = []
+    for _ in range(width):
+        current = int(random.random() * scale)
+        sign = random.choice(['-', '+'])
+        if (sign == '-'):
+            current = current * -1
+        array.append(current)
+    return array
